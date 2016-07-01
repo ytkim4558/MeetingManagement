d695bbe6dcd3638e080433334367de92ccc86b97
YongTak Kim <ytkim4558@naver.com>
2016-07-01 16:23:27 +0900

    개발자 디버그로그용 파일 생성  - 참고 : https://docs.google.com/document/d/1UVqhN33xskOS40Ulib482H60mceaBrS49c6NFHTZZds/edit?usp=sharing

---
+++ app/src/main/java/com/nagnek/android/meetingmanagement/BaseApplication.java
@@ -0,0 +1,40 @@
+package com.nagnek.android.meetingmanagement;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by yongtakpc on 2016. 7. 1..
+ */
+public class BaseApplication extends Application {
+    public static boolean DEBUG = false;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.DEBUG = isDebuggable(this);
+    }
+
+    /**
+     * get Debug Mode
+     *
+     * @param context
+     * @return
+     */
+    private boolean isDebuggable(Context context) {
+        boolean debuggable = false;
+
+        PackageManager pm = context.getPackageManager();
+        try {
+            ApplicationInfo appinfo = pm.getApplicationInfo(context.getPackageName(), 0);
+            debuggable = (0 != (appinfo.flags & ApplicationInfo.FLAG_DEBUGGABLE));
+        } catch (PackageManager.NameNotFoundException e) {
+            /* debuggable variable will remain false */
+        }
+
+        return debuggable;
+    }
+
+}
